83cd6e8e1d20251054d64cb87fe2f4b1de7e4e4d
jaundizze <105973396+jaundizze@users.noreply.github.com>
2022-05-21 17:17:58 +0530

    Add files via upload

---
+++ termux-script.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+echo "BOY:hi"
+sleep 3
+echo "GIRL:who is this"
+sleep 3
+echo "BOY:umm"
+sleep 3
+echo "GIRL:what?"
+sleep 3
+echo "BOY:is this shreya?"
+sleep 3
+echo "GIRL:yes but how do you know my name?"
+sleep 3
+echo "BOY:just by someoff my friends told me!"
+sleep 3
+echo "SHREYA:haa but what do you want?"
+sleep 3
+echo "BOY:i just wanna talk to you!"
+sleep 3
+echo "SHREYA:hurrrrr!! what's your name?"
+sleep 3
+echo "BOY:my name is ram!"
+sleep 3
+echo "SHREYA:ok ram what do you want from me?"
+sleep 3
+echo "RAM:noting but i want to say something to you!!!"
+sleep 3
+echo "SHREYA:what was that  ram!"
+sleep 3
+echo "RAM:i was watching you since 6 months, and i think i like you"
+sleep 3
+echo "SHREYA:haaaaaaaa!!!!!!!"
+sleep 3
+echo "RAM:you are so beautiful, that i cant stop watching you"
+sleep 3 
+echo "SHREYA:i don't know what to say?"
+sleep 3
+echo "RAM:take your own time, no force from my side"
+sleep 3
+echo "SHREYA:now it seems too much for me"
+sleep 3
+echo "RAM:hey!!"
+sleep 3
+echo "SHREYA:don't call me like that"
+sleep 3
+echo "RAM:okay! okay! cool down!"
+sleep 3
+echo "RAM:why so serious!"
+sleep 3 
+echo "SHREYA:hey that my favorite line from dc"
+sleep 3
+echo "RAM:what? do you watch DC movies"
+sleep 3
+echo "SHREYA:im a big fan of DC"
+sleep 3
+echo "RAM:wow im 2 min we found one similarity between us"
+sleep 3 
+echo "SHREYA:that was nice!"
+sleep 3
+echo "RAM:do you have watched all the movies of DC"
+sleep 3 
+echo "SHREYA:yes...of course!!"
+sleep 1
+echo "SHREYA:i'm such a big fan of dc, so i can't miss any of them"
+sleep 3 
+echo "RAM:my god in heaven! you are an angel that like's DC"
+sleep 3
+echo "SHREYA:hahahahahha!  you are so funny!"
+sleep 3 
+echo "RAM:i was born with it!"
+sleep 3
+echo "SHREYA:hey!"
+sleep 3 
+echo "RAM:now whos using that word heyyyyyyyyy!"
+sleep 3
+echo "SHREYA:haaaaaaaaa"
+sleep 3
+echo "RAM:haaaaaaaaaaaaaaaaaaaaaaa"
+sleep 3
+echo "SHREYA:i won't repeat"
+sleep 3 
+echo "RAM:as expected!"
+sleep 3
+echo "SHREYA:really"
+sleep 3 
+echo "RAM:you are so kind, i thought i cant start a small coversation with you"
+sleep 3
+echo "SHREYA:yaa but you are that not good as others"
+sleep 3
+echo "RAM:really"
+sleep 3
+echo "SHREYA:nevermind i was just joking"
+sleep 3
+echo "RAM:for sec my heart was started running 130 miles/hr"
+sleep 3
+echo "BACKGROUND MUSIC BE LIKE"
+play /storage/downloads/music.wav
+echo "SHREYA:did it brust"
+sleep 3
+echo "RAM:sooo funny!"
+echo "RAM:im serious!"
+sleep 3
+echo "SHREYA:okay just tell me what do you really want"
+sleep 3
+echo "RAM:you!"
+sleep 3 
+echo "SHREYA:i dont know you and i dont even seen you"
+echo "SHREYA:then how could i, you know what i mean"
+sleep 3
+echo "RAM:yaa i can undersatand you"
+sleep 3
+echo "SHREYA:haaaaaaaa"
+sleep 3
+echo "RAM:okay,then lets meet and sort out ??"
+sleep 3
+echo "SHREYA:ok,then lets make it clear"
+sleep 3
+echo "RAM:meet me at the bus stop of our college"
+sleep 3
+echo "SHREYA:how could  i identify you"
+sleep 3 
+echo "RAM:i will wear a yellow shirt"
+sleep 3
+echo "SHREYA:ok then lets make it clear"
+sleep 3 
+echo "RAM:meet you at the bus stop"
+sleep 3 
+echo "SHREYA:ok"
+sleep 3
+play /strorage/downloads/next.wav
+
+
+
